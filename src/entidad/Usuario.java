b953d851e051d45c70e9fda2619c6f81a47c88fc
Kristina Milea <Kristina Milea@192.168.0.27>
2021-01-12 18:55:46 +0100

    Quitadas las librerías innecesarias en la clase Usuario.

--- src/entidad/Usuario.java
+++ src/entidad/Usuario.java
@@ -8,22 +8,6 @@ package entidad;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
