86c0896df770531dd89495d59931f3f5c1293589
2dam <2dam@HZ341350.TartangaLH.eus>
2021-01-12 08:37:14 +0100

    Solucionado error en el controlador y el main.

--- src/controladores/UIAlumnoController.java
+++ src/controladores/UIAlumnoController.java
@@ -5,11 +5,7 @@
  */
 package controladores;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
