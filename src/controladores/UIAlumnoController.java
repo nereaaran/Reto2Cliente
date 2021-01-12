82c4518861d92e9b6e76fdbc57706579dcfe8b48
Kristina Milea <Kristina Milea@192.168.0.27>
2021-01-12 21:01:53 +0100

    Empezado el controlador de UIAlumno.

--- src/controladores/UIAlumnoController.java
+++ src/controladores/UIAlumnoController.java
@@ -5,6 +5,10 @@
  */
 package controladores;
 
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -19,14 +23,25 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author nerea
+ * @author Cristina Milea
  */
 public class UIAlumnoController {
+
+    public static final Logger LOGGER = Logger.getLogger("controladores.UIAlumnoController");
+
+    private static final int MAX_LENGHT = 50;
+    private static final int MAX_LENGHT_DNI = 9;
+
+    public static final Pattern VALID_NOMBRE = Pattern.compile("^[A-Z\\s]+$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern VALID_DNI = Pattern.compile("[A-Z]{0,1}[0-9]{7,8}[A-Z]", Pattern.CASE_INSENSITIVE);
+    public static final Pattern VALID_USUARIO = Pattern.compile("^[A-Z0-9]+$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern VALID_EMAIL = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     @FXML
     private AnchorPane paneGeneralAlumno;
@@ -124,8 +139,99 @@ public class UIAlumnoController {
         stage.setScene(scene);
 
         stage.setTitle("Gestion de alumnos");
-        //stage.setResizable(false);
-        stage.show();
+        stage.setResizable(false);
 
+        /*btnAnadir.setDisable(true);
+        btnModificar.setDisable(true);
+        btnEliminar.setDisable(true);
+        btnLimpiar.setDisable(true);
+        btnBuscar.setDisable(true);*/
+        txtNombreCompleto.textProperty().addListener(this::longitudMaximaNombreCompleto);
+        txtDni.textProperty().addListener(this::longitudMaximaDni);
+        txtUsuario.textProperty().addListener(this::longitudMaximaUsuario);
+        txtEmail.textProperty().addListener(this::longitudMaximaEmail);
+
+        stage.show();
+    }
+
+    private void longitudMaximaNombreCompleto(ObservableValue observable, String oldValue, String newValue) {
+        if (txtNombreCompleto.getText().length() > MAX_LENGHT) {
+            String nombreCompleto = txtNombreCompleto.getText().substring(0, MAX_LENGHT);
+            txtNombreCompleto.setText(nombreCompleto);
+
+            lblNombreCompletoError.setText("Nombre debe ser menor de 50 caracteres");
+            lblNombreCompletoError.setTextFill(Color.web("#FF0000"));
+        } else {
+            lblNombreCompletoError.setText("");
+
+            comprobarPatronNombre();
+        }
+    }
+
+    private void comprobarPatronNombre() {
+        if (txtNombreCompleto.getText().isEmpty()) {
+            lblNombreCompletoError.setText("");
+        } else {
+            Matcher matcher = VALID_NOMBRE.matcher(txtNombreCompleto.getText());
+            if (!matcher.find()) {
+                lblNombreCompletoError.setText("El nombre solo debe contener letras");
+                lblNombreCompletoError.setTextFill(Color.web("#FF0000"));
+            }
+        }
+    }
+
+    private void longitudMaximaDni(ObservableValue observable, String oldValue, String newValue) {
+        if (txtDni.getText().length() > MAX_LENGHT_DNI) {
+            String dni = txtDni.getText().substring(0, MAX_LENGHT_DNI);
+            txtDni.setText(dni);
+
+            lblDniError.setText("Máximo de caracteres: 9");
+            lblDniError.setTextFill(Color.web("#FF0000"));
+        } else {
+            lblDniError.setText("");
+
+            comprobarPatronDNI();
+        }
+    }
+
+    private boolean comprobarPatronDNI() {
+        boolean error = false;
+
+        if (txtDni.getText().isEmpty()) {
+            lblDniError.setText("");
+        } else {
+            Matcher matcher = VALID_DNI.matcher(txtDni.getText());
+            if (!matcher.find()) {
+                lblDniError.setText("DNI inválido");
+                lblDniError.setTextFill(Color.web("#FF0000"));
+
+                error = true;
+            }
+        }
+        return error;
+    }
+
+    private void longitudMaximaUsuario(ObservableValue observable, String oldValue, String newValue) {
+        if (txtUsuario.getText().length() == MAX_LENGHT) {
+            String usuario = txtUsuario.getText().substring(0, MAX_LENGHT);
+            txtUsuario.setText(usuario);
+
+            lblUsuarioError.setText("Usuario debe ser menor de 50 caracteres");
+            lblUsuarioError.setTextFill(Color.web("#FF0000"));
+        } else {
+            lblUsuarioError.setText("");
+        }
+    }
+
+    private void longitudMaximaEmail(ObservableValue observable, String oldValue, String newValue) {
+        if (txtEmail.getText().length() == MAX_LENGHT) {
+            String email = txtEmail.getText().substring(0, MAX_LENGHT);
+            txtEmail.setText(email);
+
+            lblEmailError.setText("Email debe ser menor de 50 caracteres");
+            lblEmailError.setTextFill(Color.web("#FF0000"));
+        } else {
+            lblEmailError.setText("");
+        }
     }
 }
