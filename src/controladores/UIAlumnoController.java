2c2c2addd9432e77628e32ff57e1950bb1361d4f
Kristina Milea <Kristina Milea@192.168.0.27>
2021-01-14 18:27:10 +0100

    Casi terminada la parte de formulario del controlador UIAlumnoController.

--- src/controladores/UIAlumnoController.java
+++ src/controladores/UIAlumnoController.java
@@ -5,12 +5,18 @@
  */
 package controladores;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.Optional;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -21,13 +27,27 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author nerea
+ * @author Cristina Milea
  */
-public class UIAlumnoController implements Initializable {
+public class UIAlumnoController {
+
+    public static final Logger LOGGER = Logger.getLogger("controladores.UIAlumnoController");
+
+    private static final int MAX_LENGHT = 50;
+    private static final int MAX_LENGHT_DNI = 9;
+
+    public static final Pattern VALID_NOMBRE = Pattern.compile("^[A-Z\\s]+$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern VALID_DNI = Pattern.compile("^[XYZ]{0,1}[0-9]{7,8}[A-Z]", Pattern.CASE_INSENSITIVE);
+    public static final Pattern VALID_USUARIO = Pattern.compile("^[A-Z0-9]+$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern VALID_EMAIL = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    private Stage stage;
 
     @FXML
     private AnchorPane paneGeneralAlumno;
@@ -114,11 +134,266 @@ public class UIAlumnoController implements Initializable {
     @FXML
     private Label lblAlumnosAsignados;
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }        
+    public void setStage(Stage primaryStage) {
+        stage = primaryStage;
+    }
+
+    public void initStage(Parent ventana) {
+        Scene scene = new Scene(ventana);
+        stage.setScene(scene);
+
+        stage.setTitle("Gestion de alumnos");
+        stage.setResizable(false);
+        txtNombreCompleto.requestFocus();
+
+        btnAnadir.setDisable(true);
+        btnModificar.setDisable(true);
+        btnEliminar.setDisable(true);
+        btnLimpiar.setDisable(true);
+
+        txtNombreCompleto.textProperty().addListener(this::comprobarLongitud);
+        txtDni.textProperty().addListener(this::comprobarLongitud);
+        txtUsuario.textProperty().addListener(this::comprobarLongitud);
+        txtEmail.textProperty().addListener(this::comprobarLongitud);
+        txtBuscarAlumno.textProperty().addListener(this::comprobarLongitud);
+
+        btnAnadir.setOnAction(this::botonAnadirPulsado);
+        btnModificar.setOnAction(this::botonModificarPulsado);
+        btnEliminar.setOnAction(this::botonEliminarPulsado);
+        btnLimpiar.setOnAction(this::botonLimpiarPulsado);
+
+        stage.show();
+    }
+
+    private boolean camposVacios() {
+        boolean vacios = false;
+
+        if (txtNombreCompleto.getText().isEmpty() || txtDni.getText().isEmpty() || txtUsuario.getText().isEmpty() || txtEmail.getText().isEmpty()) {
+            vacios = true;
+        }
+
+        return vacios;
+    }
+
+    private void habilitarBotones() {
+        if (camposVacios()) {
+            btnAnadir.setDisable(true);
+            btnModificar.setDisable(true);
+            btnEliminar.setDisable(true);
+            btnLimpiar.setDisable(true);
+        } else {
+            btnAnadir.setDisable(false);
+            btnModificar.setDisable(false);
+            btnEliminar.setDisable(false);
+            btnLimpiar.setDisable(false);
+        }
+    }
+
+    private void botonAnadirPulsado(ActionEvent event) {
+        boolean error = comprobarPatrones();
+
+        if (datePickerFechaNacimiento.getValue() == null) {
+            lblFechaNacimientoError.setText("Hay que introducir una fecha");
+            lblFechaNacimientoError.setTextFill(Color.web("#FF0000"));
+        } else if (!error) {
+            lblFechaNacimientoError.setText("OK"); //quitar mas tarde
+            lblFechaNacimientoError.setText("");
+        } else {
+            lblFechaNacimientoError.setText("NO"); //quitar mas tarde
+        }
+    }
+
+    private void botonModificarPulsado(ActionEvent event) {
+        boolean error = comprobarPatrones();
+
+        if (!error) {
+            //Hace cosas con el servidor
+        }
+    }
+
+    private void botonEliminarPulsado(ActionEvent event) {
+        //Hacer cosas con el servidor
+    }
+
+    private void botonLimpiarPulsado(ActionEvent event) {
+        lblNombreCompletoError.setText("");
+        lblDniError.setText("");
+        lblUsuarioError.setText("");
+        lblEmailError.setText("");
+        txtNombreCompleto.setText("");
+        txtDni.setText("");
+        txtUsuario.setText("");
+        txtEmail.setText("");
+
+        txtNombreCompleto.requestFocus();
+    }
+
+    private void comprobarLongitud(ObservableValue observable, String oldValue, String newValue) {
+        if (txtNombreCompleto.getText().length() > MAX_LENGHT) {
+            String nombreCompleto = txtNombreCompleto.getText().substring(0, MAX_LENGHT);
+            txtNombreCompleto.setText(nombreCompleto);
+        } else {
+            lblNombreCompletoError.setText("");
+        }
+
+        if (txtDni.getText().length() > MAX_LENGHT_DNI) {
+            String dni = txtDni.getText().substring(0, MAX_LENGHT_DNI);
+            txtDni.setText(dni);
+        } else {
+            lblDniError.setText("");
+        }
+
+        if (txtUsuario.getText().length() > MAX_LENGHT) {
+            String usuario = txtUsuario.getText().substring(0, MAX_LENGHT);
+            txtUsuario.setText(usuario);
+        } else {
+            lblUsuarioError.setText("");
+        }
+
+        if (txtEmail.getText().length() > MAX_LENGHT) {
+            String email = txtEmail.getText().substring(0, MAX_LENGHT);
+            txtEmail.setText(email);
+        } else {
+            lblEmailError.setText("");
+        }
+
+        if (txtBuscarAlumno.getText().length() > MAX_LENGHT) {
+            String buscar = txtBuscarAlumno.getText().substring(0, MAX_LENGHT);
+            txtBuscarAlumno.setText(buscar);
+        }
+
+        habilitarBotones();
+    }
+
+//--------------------------------------------------------------------------------------------------
+    private boolean comprobarPatronNombreCompleto() {
+        boolean error = false;
+
+        if (txtNombreCompleto.getText().isEmpty()) {
+            lblNombreCompletoError.setText("");
+        } else {
+            Matcher matcher = VALID_NOMBRE.matcher(txtNombreCompleto.getText());
+            if (!matcher.find()) {
+                lblNombreCompletoError.setText("El nombre solo debe contener letras");
+                lblNombreCompletoError.setTextFill(Color.web("#FF0000"));
+
+                error = true;
+            }
+        }
+
+        return error;
+    }
+
+    private boolean comprobarPatronDni() {
+        boolean error = false;
+
+        if (txtDni.getText().isEmpty()) {
+            lblDniError.setText("");
+        } else {
+            Matcher matcher = VALID_DNI.matcher(txtDni.getText());
+            if (!matcher.find()) {
+                lblDniError.setText("DNI inválido");
+                lblDniError.setTextFill(Color.web("#FF0000"));
+
+                error = true;
+            }
+        }
+
+        return error;
+    }
+
+    private boolean comprobarPatronUsuario() {
+        boolean error = false;
+
+        if (txtUsuario.getText().isEmpty()) {
+            lblUsuarioError.setText("");
+        } else {
+            Matcher matcher = VALID_USUARIO.matcher(txtUsuario.getText());
+            if (!matcher.find()) {
+                lblUsuarioError.setText("El usuario no debe contener espacios");
+                lblUsuarioError.setTextFill(Color.web("#FF0000"));
+
+                error = true;
+            }
+        }
+
+        return error;
+    }
+
+    private boolean comprobarPatronEmail() {
+        boolean error = false;
+
+        if (txtEmail.getText().isEmpty()) {
+            lblEmailError.setText("");
+        } else {
+            Matcher matcher = VALID_EMAIL.matcher(txtEmail.getText());
+            if (!matcher.find()) {
+                lblEmailError.setText("Email inválido");
+                lblEmailError.setTextFill(Color.web("#FF0000"));
+
+                error = true;
+            }
+        }
+
+        return error;
+    }
+//--------------------------------------------------------------------------------------------------
+
+    private boolean comprobarPatrones() {
+        boolean error = false;
+
+        if (comprobarPatronNombreCompleto() || comprobarPatronDni() || comprobarPatronUsuario() || comprobarPatronEmail()) {
+            error = true;
+        }
+
+        /*if (txtNombreCompleto.getText().isEmpty()) {
+            lblNombreCompletoError.setText("");
+        } else {
+            Matcher matcher = VALID_NOMBRE.matcher(txtNombreCompleto.getText());
+            if (!matcher.find()) {
+                lblNombreCompletoError.setText("El nombre solo debe contener letras");
+                lblNombreCompletoError.setTextFill(Color.web("#FF0000"));
+
+                error = true;
+            }
+        }
+
+        if (txtDni.getText().isEmpty()) {
+            lblDniError.setText("");
+        } else {
+            Matcher matcher = VALID_DNI.matcher(txtDni.getText());
+            if (!matcher.find()) {
+                lblDniError.setText("DNI inválido");
+                lblDniError.setTextFill(Color.web("#FF0000"));
+
+                error = true;
+            }
+        }
+
+        if (txtUsuario.getText().isEmpty()) {
+            lblUsuarioError.setText("");
+        } else {
+            Matcher matcher = VALID_USUARIO.matcher(txtUsuario.getText());
+            if (!matcher.find()) {
+                lblUsuarioError.setText("El usuario no debe contener espacios");
+                lblUsuarioError.setTextFill(Color.web("#FF0000"));
+
+                error = true;
+            }
+        }
+
+        if (txtEmail.getText().isEmpty()) {
+            lblEmailError.setText("");
+        } else {
+            Matcher matcher = VALID_EMAIL.matcher(txtEmail.getText());
+            if (!matcher.find()) {
+                lblEmailError.setText("Email inválido");
+                lblEmailError.setTextFill(Color.web("#FF0000"));
+
+                error = true;
+            }
+        }*/
+        return error;
+    }
+
 }
