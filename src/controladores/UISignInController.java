68f51e68335dbccd24ba5b54b9593f52a5916096
2dam <2dam@HZ341341.TartangaLH.eus>
2021-01-14 10:06:47 +0100

    Iniciado el desarrollo de los controladores de Login y Restaurar Contraseña y la clase Main.

--- src/controladores/UISignInController.java
+++ src/controladores/UISignInController.java
@@ -5,26 +5,38 @@
  */
 package controladores;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
- * FXML Controller class
+ * Controlador de la vista UISignIn que contiene los metodos para definir y
+ * controlar su comportamiento.
  *
- * @author nerea
+ * @author Nerea Aranguren
  */
-public class UISignInController implements Initializable {
+public class UISignInController {
 
+    /**
+     * Atributo estático y constante que guarda los loggers de la clase.
+     */
+    private static final Logger LOGGER = Logger.getLogger("controladores.UISignInController");
+
+    /**
+     * Lista de elementos importados de la vista FXML que representan objetos.
+     */
     @FXML
     private Pane paneSignIn;
     @FXML
@@ -49,12 +61,55 @@ public class UISignInController implements Initializable {
     private Label lblUsuarioError;
     @FXML
     private Hyperlink linkContraseñaOlvidada;
+    /**
+     * Variable de tipo stage que se usa para visualizar la ventana
+     */
+    private Stage stage;
 
     /**
-     * Initializes the controller class.
+     * Método que establece al escenario del login como escenario principal.
+     *
+     * @param primaryStage El escenario principal.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    public void setStage(Stage primaryStage) {
+        LOGGER.info("SignIn Controlador: Estableciendo stage");
+
+        stage = primaryStage;
+    }
+
+    /**
+     * Método que inicializa el escenario y los componentes del Login.
+     *
+     * @param root El objeto padre que representa el nodo root.
+     */
+    public void initStage(Parent root) {
+        LOGGER.info("SignIn Controlador: Iniciando stage");
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Sign In");
+        stage.setResizable(false);
+        
+        
+        txtUsuario.requestFocus();
+        btnIniciarSesion.setDisable(true);
+        linkContraseñaOlvidada.setOnAction(this::handleContraseñaOlvidada);
+        
+        
+        
+        
+        stage.show();
+    }
+    
+    private void handleContraseñaOlvidada(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/UIRestaurarContrasenia.fxml"));
+            Parent root = (Parent) loader.load();
+            UIRestaurarContraseniaController controller = ((UIRestaurarContraseniaController) loader.getController());
+            controller.setStage(stage);
+            controller.initStage(root);
+        } catch (IOException ex) {
+            Logger.getLogger(UILogOutController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
