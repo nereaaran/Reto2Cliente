c748a4e39736562e9e31e81ab3445678278a2c66
2dam <2dam@HZ341350.TartangaLH.eus>
2021-01-19 12:08:45 +0100

    Terminada la factoria.

--- src/factorias/UsuarioGestionFactoria.java
+++ src/factorias/UsuarioGestionFactoria.java
@@ -5,8 +5,9 @@
  */
 package factorias;
 
-import implementaciones.UsuarioGestionImplementacion;
+import implementaciones.UsuarioGestionImplementation;
 import interfaces.UsuarioGestion;
+import java.util.logging.Logger;
 
 /**
  * Clase de factoría que gestiona los usuarios.
@@ -16,12 +17,19 @@ import interfaces.UsuarioGestion;
 public class UsuarioGestionFactoria {
 
     /**
+     * Atributo estático y constante que guarda los loggers de la clase.
+     */
+    private static final Logger LOGGER = Logger.getLogger("implementaciones.UsuarioGestionFactoria");
+
+    /**
      * Método que crea una nueva implementación para la gestión de usuarios.
      *
      * @return la implementación.
      */
     public static UsuarioGestion crearUsuarioGestion() {
-        UsuarioGestion usuarioGestion = new UsuarioGestionImplementacion();
+        LOGGER.info("UsuarioGestionFactoria: Creando la implementacion");
+
+        UsuarioGestion usuarioGestion = new UsuarioGestionImplementation();
 
         return usuarioGestion;
     }
