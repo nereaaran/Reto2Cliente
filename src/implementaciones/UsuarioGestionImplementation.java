2bc95da1e0d471c0843fb6cc552e8f58ef2d5e9b
2dam <2dam@HZ341350.TartangaLH.eus>
2021-01-21 09:57:05 +0100

    Añadido Javadoc.

--- src/implementaciones/UsuarioGestionImplementation.java
+++ src/implementaciones/UsuarioGestionImplementation.java
@@ -8,66 +8,155 @@ package implementaciones;
 import entidad.Usuario;
 import interfaces.UsuarioGestion;
 import java.util.Collection;
+import java.util.logging.Logger;
 import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.core.GenericType;
 import rest.UsuarioRESTClient;
 
 /**
- * 
+ * Clase que implementa la interfaz UsuarioGestion usando un cliente web
+ * RESTful.
+ *
  * @author Cristina Milea
  */
 public class UsuarioGestionImplementation implements UsuarioGestion {
 
+    /**
+     * Atributo estático y constante que guarda los loggers de la clase.
+     */
+    private static final Logger LOGGER = Logger.getLogger("implementaciones.LibroGestionImplementation");
+
+    /**
+     * Cliente web de REST usuario.
+     */
     private final UsuarioRESTClient webClient;
 
+    /**
+     * Constructor que sirve para acceder al servicio REST del lado servidor.
+     */
     public UsuarioGestionImplementation() {
         webClient = new UsuarioRESTClient();
     }
 
+    /**
+     * Manda una petición REST de tipo create al servidor y se crea un nuevo
+     * usuario.
+     *
+     * @param usuario el usuario que se creará.
+     */
     @Override
-    public void create(Usuario usuario) throws ClientErrorException {
-        webClient.create(usuario);
+    public void create(Usuario usuario) {
+        try {
+            LOGGER.info("UsuarioGestionImplementation: Creando usuario");
+            webClient.create(usuario);
+        } catch (ClientErrorException e) {
+            LOGGER.severe(e.getMessage());
+        }
     }
 
+    /**
+     * Manda una petición REST de tipo edit al servidor y modifica el usuario.
+     *
+     * @param usuario el usuario que se modificará.
+     */
     @Override
     public void edit(Usuario usuario) throws ClientErrorException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Manda una petición REST de tipo remove al servidor y elimina el usuario
+     * por el id.
+     *
+     * @param id el id del usuario que se eliminará.
+     */
     @Override
     public void remove(Integer id) throws ClientErrorException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Manda una petición REST de tipo find al servidor y busca el usuario por
+     * el id.
+     *
+     * @param id el id del usuario por el que buscará.
+     */
     @Override
     public Usuario find(Integer id) throws ClientErrorException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Manda una petición REST para que busque un usuario por login al servidor
+     * y busca el usuario.
+     *
+     * @param login el login del usuario por el que se buscará.
+     * @return la colección de usuarios que encuentra.
+     */
     @Override
-    public Collection<Usuario> buscarUsuarioPorLogin(String login) throws ClientErrorException {
-        Collection<Usuario> usuario = webClient.buscarUsuarioPorLogin(new GenericType<Collection<Usuario>>() {}, login);
+    public Collection<Usuario> buscarUsuarioPorLogin(String login) {
+        Collection<Usuario> usuario = null;
+        try {
+            usuario = webClient.buscarUsuarioPorLogin(new GenericType<Collection<Usuario>>() {
+            }, login);
+        } catch (ClientErrorException e) {
+            LOGGER.severe(e.getMessage());
+        }
 
         return usuario;
     }
 
+    /**
+     * Manda una petición REST para que busque un usuario por email al servidor
+     * y busca el usuario.
+     *
+     * @param email el email del usuario por el que se buscará.
+     * @return la colección de usuarios que encuentra.
+     */
     @Override
-    public Collection<Usuario> buscarUsuarioPorEmail(String email) throws ClientErrorException {
-        Collection<Usuario> usuario = webClient.buscarUsuarioPorEmail(new GenericType<Collection<Usuario>>() {}, email);
+    public Collection<Usuario> buscarUsuarioPorEmail(String email) {
+        Collection<Usuario> usuario = null;
+        try {
+            usuario = webClient.buscarUsuarioPorEmail(new GenericType<Collection<Usuario>>() {
+            }, email);
+        } catch (ClientErrorException e) {
+            LOGGER.severe(e.getMessage());
+        }
 
         return usuario;
     }
 
+    /**
+     * Manda una petición REST para que busque un usuario por login y contraseña
+     * al servidor y busca el usuario.
+     *
+     * @param login el login del usuario por el que se buscará.
+     * @param contrasenia la contraseña del usuario por el que se buscará.
+     * @return la colección de usuarios que encuentra.
+     */
     @Override
     public Collection<Usuario> buscarLoginYContrasenia(String login, String contrasenia) throws ClientErrorException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Manda una petición REST para que busque todos los usuarios de tipo alumno
+     * al servidor y busca el usuario.
+     *
+     * @return la colección de usuarios que encuentra.
+     */
     @Override
     public Collection<Usuario> consultarTodosAlumnos() throws ClientErrorException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Manda una petición REST para que busque un usuario de tipo alumno por el
+     * nombre al servidor y busca el usuario.
+     *
+     * @param nombre el nombre del alumno.
+     * @return la colección de usuarios que encuentra.
+     */
     @Override
     public Collection<Usuario> buscarAlumnoPorNombre(String nombre) throws ClientErrorException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
--- src/rest/UsuarioRESTClient.java
+++ src/rest/UsuarioRESTClient.java
@@ -25,47 +25,122 @@ import javax.ws.rs.core.GenericType;
  */
 public class UsuarioRESTClient {
 
+    /**
+     * Objeto de tipo webTarget.
+     */
     private final WebTarget webTarget;
+    /**
+     * Objeto de tipo Client.
+     */
     private final Client client;
+    /**
+     * Coge el URI de un archivo de propiedades.
+     */
     private static final String BASE_URI = "http://localhost:8080/Reto2Servidor/webresources";
 
+    /**
+     * Constructor que crea un cliente web RESTful y establece la ruta del
+     * objeto webTarget asociado al cliente.
+     */
     public UsuarioRESTClient() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
         webTarget = client.target(BASE_URI).path("usuario");
     }
-    
+
+    /**
+     * Crea una reperesentación XML de la entidad Usuario y la manda al servicio
+     * web usuario RESTful como petición para modificarlo.
+     *
+     * @param requestEntity Objeto con los datos a ser modificados.
+     * @throws ClientErrorException Si hay un error durante el proceso. El error
+     * va envuelto en una respuesta de error de HTTP.
+     */
     public void edit(Object requestEntity) throws ClientErrorException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
 
+    /**
+     * Obtiene una reperesentación XML de la entidad Usuario del servicio web
+     * usuario RESTful y lo devuelve como un objeto de tipo genérico.
+     *
+     * @param <T> Clase de tipo generico.
+     * @param responseType La clase objeto de la instancia de retorno.
+     * @param id La id de la instancia del lado servidor.
+     * @return El objeto con los datos.
+     * @throws ClientErrorException Si hay un error durante el proceso. El error
+     * va envuelto en una respuesta de error de HTTP.
+     */
     public <T> T find(Class<T> responseType, Integer id) throws ClientErrorException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
+    /**
+     * Crea una reperesentación XML de la entidad Usuario y la manda al servicio
+     * web usuario RESTful como petición para crearlo.
+     *
+     * @param requestEntity Objeto con los datos a ser creados.
+     * @throws ClientErrorException Si hay un error durante el proceso. El error
+     * va envuelto en una respuesta de error de HTTP.
+     */
     public void create(Object requestEntity) throws ClientErrorException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
 
+    /**
+     * Manda al servicio web usuario RESTful una petición para eliminar un
+     * usuario identificado por el id.
+     *
+     * @param id El id de la entidad usuario que se eliminará.
+     * @throws ClientErrorException Si hay un error durante el proceso. El error
+     * va envuelto en una respuesta de error de HTTP.
+     */
     public void remove(String id) throws ClientErrorException {
         webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{id})).request().delete();
     }
-    
+
+    /**
+     * Obtiene una lista de reperesentaciónes XML de la entidad Usuario del
+     * servicio web usuario RESTful y lo devuelve como un objeto de tipo
+     * genérico.
+     *
+     * @param <T> Clase de tipo generico.
+     * @param responseType La clase objeto de la instancia de retorno.
+     * @param login El login de la instancia del lado servidor.
+     * @return Coleccion de objetos con los datos.
+     * @throws ClientErrorException Si hay un error durante el proceso. El error
+     * va envuelto en una respuesta de error de HTTP.
+     */
     public <T> T buscarUsuarioPorLogin(GenericType<T> responseType, String login) throws ClientErrorException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("login/{0}", new Object[]{login}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
-    
+
+    /**
+     * Obtiene una lista de reperesentaciónes XML de la entidad Usuario del
+     * servicio web usuario RESTful y lo devuelve como un objeto de tipo
+     * genérico.
+     *
+     * @param <T> Clase de tipo generico.
+     * @param responseType La clase objeto de la instancia de retorno.
+     * @param email El email de la instancia del lado servidor.
+     * @return Coleccion de objetos con los datos.
+     * @throws ClientErrorException Si hay un error durante el proceso. El error
+     * va envuelto en una respuesta de error de HTTP.
+     */
     public <T> T buscarUsuarioPorEmail(GenericType<T> responseType, String email) throws ClientErrorException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("email/{0}", new Object[]{email}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
+    /**
+     * Cierra el servicio web RESTful del cliente.
+     */
     public void close() {
         client.close();
     }
-    
+
 }
