4a6f8409e98b0d20c4b7b1ed5de03afa9b51aa37
jony-ecua <jvc.ecua@gmail.com>
2021-01-25 03:44:01 +0100

    desarrollo controlador UIGrupoController avamzado sin terminar

--- src/controladores/UIGrupoController.java
+++ src/controladores/UIGrupoController.java
@@ -5,28 +5,50 @@
  */
 package controladores;
 
-
 import entidad.Grupo;
+import entidad.GrupoLibro;
+import entidad.Libro;
+import factorias.GestionFactoria;
 import implementaciones.GrupoGestionImplementation;
 import interfaces.GrupoGestion;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javafx.application.Platform;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javax.ws.rs.core.GenericType;
+import rest.GrupoRESTClient;
 
 /**
  * FXML Controller class
@@ -34,6 +56,12 @@ import javafx.stage.Stage;
  * @author Jonathan Viñan
  */
 public class UIGrupoController {
+
+    private static final int MAX_LENGHT_NOMBRE_GRUPO = 100;
+    private static final int MAX_LENGHT_DESCRIPCION = 200;
+    private static final int MAX_LENGHT_CANTIDAD_TOTAL = 2;
+    public static final Pattern VALIDAR_ALFANUMERICO = Pattern.compile("^[A-Z0-9]+$", Pattern.CASE_INSENSITIVE);
+    public static final Pattern VALIDAR_ES_NUMERO = Pattern.compile("^[0-9]", Pattern.CASE_INSENSITIVE);
 
     @FXML
     private AnchorPane paneGeneralGrupo;
@@ -64,6 +92,8 @@ public class UIGrupoController {
     @FXML
     private Button btnBuscar;
     @FXML
+    private Button btnConsultarLibros;
+    @FXML
     private MenuBar munuBar;
     @FXML
     private MenuItem mnMiPerfil;
@@ -78,15 +108,13 @@ public class UIGrupoController {
     @FXML
     private AnchorPane paneInferiorGrupo;
     @FXML
-    private TableView<?> tablaGrupo;
+    private TableView<Grupo> tablaGrupo;
     @FXML
-    private TableColumn<?, ?> nombreGrupoCL;
+    private TableColumn<Grupo, String> nombreGrupoCL;
     @FXML
-    private TableColumn<?, ?> descripcionCL;
+    private TableColumn<Grupo, String> descripcionCL;
     @FXML
-    private TableColumn<?, ?> numAlumnosCL;
-    @FXML
-    private TableColumn<?, ?> nombreProfesorCL;
+    private TableColumn<Grupo, Integer> numAlumnosCL;
     @FXML
     private TableView<?> tablaLibro1;
     @FXML
@@ -103,24 +131,20 @@ public class UIGrupoController {
     private Button btnAnadirLibro;
     @FXML
     private Button btnEliminarLibro;
-    
-    private static final Logger LOGGER = Logger.getLogger(UIGrupoController.class.getName());
-            
-     ObservableList<Grupo> grupos;
 
-    private void anadirGrupo(ActionEvent event){
-        Grupo grupo = new Grupo();
-        
-        grupo.setNombre(txtNombreGrupo.getText());
-        grupo.setDescripcion(txtDescripcion.getText());
-        grupo.setNumAlumno(Integer.MIN_VALUE);
-        grupos.add(grupo);
-    }
-    
-     /**
+    private GrupoGestionImplementation gestionImplementation;
+    private GrupoGestion grupoGestion = GestionFactoria.getGrupoGestion();
+    private ObservableList<Grupo> listarGrupos;
+    private ObservableList<GrupoLibro> listarGrupoLibro;
+
+    //ObservableList listaGrupos = FXCollections.observableArrayList();
+    private static final Logger LOGGER = Logger.getLogger(UIGrupoController.class.getName());
+
+    /**
      * Variable de tipo stage que se usa para visualizar la ventana
      */
     private Stage stage;
+
     /**
      * Método que establece al escenario del login como escenario principal.
      *
@@ -138,30 +162,226 @@ public class UIGrupoController {
      * @param root El objeto padre que representa el nodo root.
      */
     public void initStage(Parent root) {
-        LOGGER.info("Grupo Controlador: Iniciando stage");
-
+        LOGGER.info("Grupo Controlador: Iniciando");
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Gestion de grupos");
         stage.setResizable(false);
-        
-        
+        stage.onCloseRequestProperty().set(this::cerrarVentana);
+
+        //Iniciando las columnas de la tabla Grupo
+        nombreGrupoCL.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        descripcionCL.setCellValueFactory(new PropertyValueFactory<>("descripcion"));
+        numAlumnosCL.setCellValueFactory(new PropertyValueFactory<>("numAlumno"));
+
+        //Propiedades de cambio de texto
+        txtNombreGrupo.textProperty().addListener(this::handleTextoCambiado);
+        txtDescripcion.textProperty().addListener(this::handleTextoCambiado);
+
+        GrupoGestion grupoGestion = new GrupoGestionImplementation();
+        listarGrupos = FXCollections.observableArrayList(new ArrayList<>(grupoGestion.listarGrupos()));
+        tablaGrupo.setItems(listarGrupos);
+
         txtNombreGrupo.requestFocus();
-        //btnIniciarSesion.setDisable(true);
-        //linkContraseñaOlvidada.setOnAction(this::handleContraseñaOlvidada);
-        //btnIniciarSesion.setOnAction(this::handleIniciarSesion);
-        
-        
-        
+        btnAnadir.setDisable(false);
+        btnModificar.setDisable(true);
+        btnEliminar.setDisable(true);
+        btnLimpiar.setDisable(true);
+        btnBuscar.setDisable(true);
+        btnGestionarAlumno.setDisable(true);
+        btnEliminarLibro.setDisable(true);
+        btnGestionarAlumno.setDisable(false);
+        btnConsultarLibros.setDisable(false);
+
+        //Acciones de los botnes
+        btnAnadir.setOnAction(this::anadirGrupos);
+        btnModificar.setOnAction(this::modificarGrupo);
+        //btnEliminar.setOnAction(this::eliminarGrupo);
+        btnLimpiar.setDisable(true);
+        btnBuscar.setDisable(true);
+        btnGestionarAlumno.setDisable(true);
+        btnEliminarLibro.setDisable(true);
+        btnGestionarAlumno.setDisable(false);
+        btnConsultarLibros.setDisable(false);
+
         stage.show();
     }
-    
-    public void handleGrupoInitialize(ActionEvent event) {
-           
-        GrupoGestion grupoGestion = new GrupoGestionImplementation();
-        
-        // TODO
+
+    public GrupoGestionImplementation getGestionImplementation() {
+        return this.getGestionImplementation();
     }
-    
-    
+
+    private void anadirGrupos(ActionEvent event) {
+        try {
+            Grupo nuevoGrupo = new Grupo();
+            nuevoGrupo.setNombre(txtNombreGrupo.getText());
+            nuevoGrupo.setDescripcion(txtDescripcion.getText());
+            nuevoGrupo.setNumAlumno(0);
+
+            grupoGestion.create(nuevoGrupo);
+            tablaGrupo.getItems().add(nuevoGrupo);
+            tablaGrupo.refresh();
+        } catch (Exception e) {
+            LOGGER.severe(e.getMessage());
+        }
+    }
+
+    private void modificarGrupo(ActionEvent event) {
+        if (comprobarCampos()) {
+            ///////////////////////////////////////////////
+            limpiarCamposTexto();
+        }
+    }
+
+    private void seleccionDatoTabla(ObservableValue observable, Object oldValue, Object newValue) {
+        if (newValue != null) {
+            Grupo grupo = (Grupo) newValue;
+            txtNombreGrupo.setText(grupo.getNombre());
+            txtDescripcion.setText(grupo.getDescripcion());
+            btnAnadir.setDisable(true);
+        }
+    }
+
+    private void cerrarVentana(WindowEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+
+        alert.setTitle("Salir");
+        alert.setHeaderText(null);
+        alert.setContentText("¿Seguro que quieres cerrar la ventana?");
+
+        alert.getButtonTypes().setAll(ButtonType.OK, ButtonType.CANCEL);
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get().equals(ButtonType.OK)) {
+            LOGGER.info("Grupo Controlador: Cerrando aplicacion");
+            stage.close();
+            Platform.exit();
+        } else {
+            event.consume();
+            alert.close();
+        }
+    }
+
+    private void handleTextoCambiado(ObservableValue observable, String oldValue, String newValue) {
+        StringProperty textProperty = (StringProperty) observable;
+        TextField changedTextField = (TextField) textProperty.getBean();
+        String changedTextFieldName = changedTextField.getId();
+
+        textFieldOverMaxLength(changedTextField, changedTextFieldName);
+
+        habilitarBotones();
+    }
+
+    private boolean comprobarCampos() {
+        boolean error = false;
+
+        if (txtNombreGrupo.getText().isEmpty()) {
+            lblNombreGrupoError.setText("");
+        } else {
+            Matcher matcher = VALIDAR_ALFANUMERICO.matcher(txtNombreGrupo.getText());
+            if (!matcher.find()) {
+                lblNombreGrupoError.setText("El nombre sólo debe contener alfanumericos");
+                lblNombreGrupoError.setTextFill(Color.web("#FF0000"));
+
+                error = true;
+            } else {
+                lblNombreGrupoError.setText("");
+            }
+        }
+
+        if (txtDescripcion.getText().isEmpty()) {
+            lblDescripcionError.setText("");
+        } else {
+            Matcher matcher = VALIDAR_ALFANUMERICO.matcher(txtDescripcion.getText());
+            if (!matcher.find()) {
+                lblDescripcionError.setText("Descripcion debe contener alfanumericos");
+                lblDescripcionError.setTextFill(Color.web("#FF0000"));
+
+                error = true;
+            } else {
+                lblDescripcionError.setText("");
+            }
+        }
+
+        return error;
+
+    }
+
+    /*private void eliminarGrupo() {
+        Grupo SeleccionadoGrupo = ((Grupo) tablaGrupo.getSelectionModel().getSelectedItem());
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Eliminar");
+        alert.setHeaderText(null);
+        alert.setResizable(false);
+        alert.setContentText("¿Seguro que quieres eliminar este grupo?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        ButtonType button = result.orElse(ButtonType.CANCEL);
+
+        if (button == ButtonType.OK) {
+            grupoGestion.remove(SeleccionadoGrupo);
+
+            tablaGrupo.getItems().remove(SeleccionadoGrupo);
+            tablaGrupo.refresh();
+
+            limpiarCamposTexto();
+        }
+    } */
+
+    private void limpiarCamposTexto() {
+        txtNombreGrupo.setText("");
+        txtDescripcion.setText("");
+    }
+
+    private void textFieldOverMaxLength(TextField changedTextField, String changedTextFieldName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void habilitarBotones() {
+        if (camposVacios()) {
+            btnAnadir.setDisable(true);
+            btnModificar.setDisable(true);
+            btnEliminar.setDisable(true);
+        } else {
+            btnAnadir.setDisable(false);
+            btnModificar.setDisable(false);
+            btnEliminar.setDisable(false);
+        }
+
+        if (camposNoVacios()) {
+            btnLimpiar.setDisable(false);
+        } else {
+            btnLimpiar.setDisable(true);
+        }
+
+        if (txtBuscarGrupo.getText().isEmpty()) {
+            listarGrupos = FXCollections.observableArrayList(grupoGestion.listarGrupos());
+            tablaGrupo.setItems(listarGrupos);
+            btnBuscar.setDisable(true);
+        } else {
+            btnBuscar.setDisable(false);
+        }
+    }
+
+    private boolean camposVacios() {
+        boolean vacio = false;
+
+        if (txtNombreGrupo.getText().isEmpty() || txtDescripcion.getText().isEmpty() || txtBuscarGrupo.getText().isEmpty()) {
+            vacio = true;
+        }
+
+        return vacio;
+    }
+
+    private boolean camposNoVacios() {
+        boolean vacio = false;
+
+        if (!txtNombreGrupo.getText().isEmpty() || !txtDescripcion.getText().isEmpty() || !txtBuscarGrupo.getText().isEmpty()) {
+            vacio = true;
+        }
+
+        return vacio;
+    }
+
 }
